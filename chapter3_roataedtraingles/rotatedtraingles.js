ae5d4f40217833c511eb092c34f171948943728a
tuxianchao <xianchaot@gmail.com>
2019-05-08 10:09:31 +0800

    TODO:三角形几何旋转,用原始的方式(三角函数计算)旋转图形

--- chapter3_roataedtraingles/rotatedtraingles.js
+++ chapter3_roataedtraingles/rotatedtraingles.js
@@ -1,51 +1,34 @@
-
 window.onload = () => {
     const VSHADER_SOURCE =
         'attribute vec4 a_Position;\n' +
         'void main(){\n' +
         'gl_Position = a_Position;\n' +
-        // 'gl_PointSize = 10.0;\n' +
         '}\n';
-
     const FSHADER_SOURCE =
         'void main(){\n' +
         'gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n' +
-        '}\n';
+        '}';
     const canvas = document.querySelector('#glCanvas');
     const gl = getWebGLContext(canvas);
+
     if (gl < 0) {
-        console.error(`failed to init webgl context`);
+        console.error('failed to init webgl context.');
         return;
     }
-
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.error('failed to compile shader source or link shder program');
+        console.error('failed compile shader source or link shader program.');
         return;
     }
+    const n = initVertexBuffers(gl);
 
-    // 获取顶点缓冲对象
-    let n = initVertexBuffers(gl);
-    if (n < 0) {
-        console.error('failed to set position of vertices');
-        return;
-    }
-    ///设置背景色
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    //一次性画三个点,数据来源于传递给顶点缓冲对象
-    //  绘制三角形
     gl.drawArrays(gl.TRIANGLES, 0, n);
-
-    // 实验
-    // gl.drawArrays(gl.LINES, 0, n);
-    // gl.drawArrays(gl.LINE_STRIP, 0, n);
-    // gl.drawArrays(gl.LINE_LOOP, 0, n);
 }
 
 /**
- * 填充顶点缓冲对象
- * @param {webgl} gl 
+ * 初始化顶点数据
  */
 function initVertexBuffers(gl) {
     const vertices = new Float32Array([
@@ -53,23 +36,18 @@ function initVertexBuffers(gl) {
         -0.5, -0.5,
         0.5, -0.5
     ]);
-    //三个点
+
     const pointCount = 3;
     const vertexBuffer = gl.createBuffer();
     if (!vertexBuffer) {
-        console.error('failed to creat buffer object.');
+        console.error('faile to create vertex buffer object.');
         return -1;
     }
-
-    //将缓冲区绑定到opengl目标
+    // 顶点数据处理
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    //  向缓冲区传递数据
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-    //将顶点缓冲的数据分配给a_Position变量
-    let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-
-    // 连接a_Position和分配给他的缓冲区对象
     gl.enableVertexAttribArray(a_Position);
 
     return pointCount;
