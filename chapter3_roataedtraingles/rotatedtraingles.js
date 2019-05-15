e02db72327ca99349d1fcf68cdc52eb4e90a623d
tuxianchao <xianchaot@gmail.com>
2019-05-15 18:31:50 +0800

    运行三角函数计算实现图形的旋转,计算顶点旋转后的值.

--- chapter3_roataedtraingles/rotatedtraingles.js
+++ chapter3_roataedtraingles/rotatedtraingles.js
@@ -1,13 +1,31 @@
+// 三角形绕着z轴旋转B度
+
+// x' = x cos b - y sin b 
+// y' = x sin b + y cos b
+// z' = z
+
+// 旋转角度
+const ANGLE = 90.0;
 window.onload = () => {
     const VSHADER_SOURCE =
+        // x' = x cos b - y sin b 
+        // y' = x sin b + y cos b
+        // z' = z
         'attribute vec4 a_Position;\n' +
+        'uniform float u_CosB, u_SinB;\n' +
         'void main(){\n' +
         'gl_Position = a_Position;\n' +
+        'gl_Position.x = a_Position.x * u_CosB - a_Position.y * u_SinB;\n' +
+        'gl_Position.y = a_Position.x * u_SinB + a_Position.y * u_CosB;\n' +
+        'gl_Position.z = a_Position.z;\n' +
+        'gl_Position.w = 1.0;\n' +
         '}\n';
+
     const FSHADER_SOURCE =
         'void main(){\n' +
         'gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n' +
         '}';
+
     const canvas = document.querySelector('#glCanvas');
     const gl = getWebGLContext(canvas);
 
@@ -19,6 +37,26 @@ window.onload = () => {
         console.error('failed compile shader source or link shader program.');
         return;
     }
+
+    // 传递两个uniform进去
+    // 转换为弧度制 
+    let radian = Math.PI * ANGLE / 180.0;
+    let sinB = Math.sin(radian);
+    let cosB = Math.cos(radian);
+    console.debug(sinB);
+    console.debug(cosB);
+    let u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
+    let u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
+    if (!u_SinB || !u_CosB) {
+        console.error('Failed to get the storage uniform location!');
+        return;
+    }
+    console.debug(u_SinB, u_CosB);
+
+    gl.uniform1f(u_CosB, cosB);
+    gl.uniform1f(u_SinB, sinB);
+
+
     const n = initVertexBuffers(gl);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
