54f19e250ff551efbc9da8a6414dfe6e6cc04574
tuxianchao <xianchaot@gmail.com>
2019-05-28 23:47:51 +0800

    完成加载多张纹理

--- chapter5_multiTexture/multiTexture.js
+++ chapter5_multiTexture/multiTexture.js
@@ -9,15 +9,20 @@ window.onload = () => {
     }
     `;
     const FSHADER_SOURCE = `
+    #ifdef GL_ES
+        precision mediump float;
+    #endif
     uniform sampler2D u_Sampler0;// 纹理1
     uniform sampler2D u_Sampler1;// 纹理2
     varying vec2 v_TexCoord;
     void main(){
-
+        vec4 color0 = texture2D(u_Sampler0,v_TexCoord);
+        vec4 color1 = texture2D(u_Sampler1,v_TexCoord);
+        gl_FragColor = color0 * color1;
     }
     `;
 
-    const canvas = document.querySelector('#canvas');
+    const canvas = document.querySelector('#glCanvas');
     const gl = getWebGLContext(canvas);
 
     if (gl < 0) {
@@ -32,9 +37,67 @@ window.onload = () => {
 
 
     const n = initVertexBuffer(gl);
+    initTexture(gl, n);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function initTexture(gl, n) {
+    let texture0 = gl.createTexture();
+    let texture1 = gl.createTexture();
+
+    if (!texture0 || !texture1) {
+        console.error('failed to create texture object.');
+        return false;
+    }
+    // 获取shader中两种纹理的位置
+    const u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+    const u_Sampler1 = gl.getUniformLocation(gl.program, 'u_Sampler1');
+
+    let image0 = new Image();
+    let image1 = new Image();
+
+    image0.src = '../resources/sky.jpg';
+    image1.src = '../resources/circle.gif';
+
+    image0.onload = function () {
+        // 使用第一个纹理单元
+        loadTexture(gl, n, texture0, u_Sampler0, image0, 0);
+    };
+    image1.onload = function () {
+        // 使用第二个纹理单元
+        loadTexture(gl, n, texture1, u_Sampler1, image1, 1);
+    };
+    return true;
+}
+
+// 两个纹理单元是否准备好
+let g_texUnit0 = false;
+let g_texUnit1 = false;
+function loadTexture(gl, n, texture, u_Sampler, image, texUnit) {
+    if (texUnit === 0) {
+        g_texUnit0 = true;
+        gl.activeTexture(gl.TEXTURE0);
+    } else if (texUnit === 1) {
+        g_texUnit1 = true;
+        gl.activeTexture(gl.TEXTURE1);
+    }
+
+    // 当前已经指定好了纹理单元,只需要配置纹理参数和纹理图片就好了
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // 配置纹理参数
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    // 配置纹理图片
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+    // 将对应的纹理传递给片元shader
+    gl.uniform1i(u_Sampler, texUnit);
+
+    if (g_texUnit0 && g_texUnit1) {
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+    }
 }
 
 function initVertexBuffer(gl) {
@@ -44,13 +107,12 @@ function initVertexBuffer(gl) {
         -0.5, -0.5, 0.0, 0.0,
         0.5, 0.5, 1.0, 1.0,
         0.5, -0.5, 1.0, 0.0,
-
     ]);
 
     const pointCount = 4;//顶点数
-    const vertextBuffer = gl.createBuffer();
+    const vertexBuffer = gl.createBuffer();
 
-    if (!vertextBuffer) {
+    if (!vertexBuffer) {
         console.error('faile to create vertex buffer object.');
         return -1;
     }
@@ -63,9 +125,13 @@ function initVertexBuffer(gl) {
 
     // 写顶点坐标
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, GL_FLOAT_SIZE * 4, 0);
+    gl.enableVertexAttribArray(a_Position);
 
     // 写纹理坐标
     const a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
-    
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, GL_FLOAT_SIZE * 4, GL_FLOAT_SIZE * 2);
+    gl.enableVertexAttribArray(a_TexCoord);
 
+    return pointCount;
 }
