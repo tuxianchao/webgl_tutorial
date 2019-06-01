99d139c6ad5dbf12d8412b774c10f7889f1d2526
tuxianchao <xianchaot@gmail.com>
2019-06-01 18:35:05 +0800

    将之前不在可视范围的内容显示出来

--- chapter7_LookAtTrianlesWithKey_viewVolume/LookAttrianglesWithKey.js
+++ chapter7_LookAtTrianlesWithKey_viewVolume/LookAttrianglesWithKey.js
@@ -1,69 +1,79 @@
 window.onload = _ => {
+
     const VSHADER_SOURCE = `
     attribute vec4 a_Position;
     attribute vec4 a_Color;
-    uniform mat4 u_ProjMatrix;
     varying vec4 v_Color;
+    uniform mat4 u_ViewMatrix; // 视图矩阵
+    uniform mat4 u_ProjMatrix; // 正交投影矩阵
     void main(){
-        gl_PointSize = 10.0;
         v_Color = a_Color;
-        gl_Position = u_ProjMatrix * a_Position;
+        gl_PointSize = 10.0;
+        gl_Position = u_ProjMatrix * u_ViewMatrix * a_Position;
     }
     `;
     const FSHADER_SOURCE = `
     precision mediump float;
     varying vec4 v_Color;
     void main(){
-        gl_FragColor= v_Color;
+        gl_FragColor = v_Color;
     }
     `;
 
-    const canvas = document.querySelector(`#glCanvas`);
+    const canvas = document.querySelector('#glCanvas');
     const gl = getWebGLContext(canvas);
 
     if (gl < 0) {
         console.error(`failed to init webgl context.`);
         return;
     }
-
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-        console.error(`failed to compile shader or link shader program`);
+        console.error(`failed to compile shader or link shader program.`);
         return;
     }
-
     const n = initVertexBuffer(gl);
 
-    // 获取正交投影矩阵变量
-    const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
+    const u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+    let viewMatrix = new Matrix4();
 
-    const projMatrix = new Matrix4();
-    // 第一次draw一屏初始化状态
-    draw(gl, n, u_ProjMatrix, projMatrix);
+    //  再传给shader一个正交矩阵
+    const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
+    const projView = new Matrix4();
+    projView.setOrtho(-1.0, 1.0, -1.0, 1.0, 0.0, 2.0);
+
+    gl.uniformMatrix4fv(u_ProjMatrix, false, projView.elements);
+    
+    // 预先画一屏
+    draw(gl, n, u_ViewMatrix, viewMatrix);
+    // 注册监听函数
     document.onkeydown = event => {
-        switch (event.keyCode) {
-            case 39: near += 0.01; break; //right
-            case 37: near -= 0.01; break; //left
-            case 38: far += 0.01; break; //up
-            case 40: far -= 0.01; break; //down
-            default: return;
+        if (event.keyCode === 39) {
+            // 按下右键
+            eyeX += 0.01;
+            console.log(`right`);
+        } else if (event.keyCode === 37) {
+            // 按下左键
+            eyeX -= 0.01;
+            console.log(`left`);
+        } else {
+            console.log(event);
+            return;
         }
-        draw(gl, n, u_ProjMatrix, projMatrix);
+        viewMatrix.setLookAt
+        // draw函数
+        draw(gl, n, u_ViewMatrix, viewMatrix);
     }
 
 }
-// 近裁面和远裁面参数
-let near = 0.0, far = 0.5;
-function draw(gl, n, u_ProjMatrix, projMatrix) {
-    // 参数 近裁面的左右边界,远裁面的左右边界,近裁面和远裁面的位置
-    projMatrix.setOrtho(-1, 1, -1, 1, near, far);
-
-    //将视图矩阵传递给u_ViewMatrix变量
-    gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
-    
+// 视点位置
+let eyeX = 0.20, eyeY = 0.25, eyeZ = 0.25;
+function draw(gl, n, u_ViewMatrix, viewMatrix) {
+    // 设置试点和视线
+    viewMatrix.setLookAt(eyeX, eyeY, eyeZ, 0, 0, 0, 0, 1, 0);
+    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COlOR_BUFFER_BIT);
-    document.querySelector('#nearFar').innerHTML =
-        `near: ${Math.round(near * 100) / 100} , far: ${Math.round(far * 100) / 100}`;
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
     gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
@@ -79,22 +89,36 @@ function initVertexBuffer(gl) {
 
         0.0, 0.5, 0.0, 0.4, 0.4, 1.0,  // The front blue one
         -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
-        0.5, -0.5, 0.0, 1.0, 0.4, 0.4
+        0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
     ]);
 
     const pointCount = 9;
+
     const vertexBuffer = gl.createBuffer();
-    const GL_FLOAT_SIZE = verticesColors.BYTES_PER_ELEMENT;
+    if (!vertexBuffer) {
+        console.warn(`failed to create vertex object buffer`);
+        return -1;
+    }
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
 
+    const GL_FLOAT_SIZE = verticesColors.BYTES_PER_ELEMENT;
     // vertex
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.warn(`failed to get location of a_Position.`);
+        return -1;
+    }
     gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, GL_FLOAT_SIZE * 6, 0);
     gl.enableVertexAttribArray(a_Position);
+
     // color
     const a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+    if (a_Color < 0) {
+        console.warn(`failed to get location of a_Color.`);
+        return -1;
+    }
     gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, GL_FLOAT_SIZE * 6, GL_FLOAT_SIZE * 3);
     gl.enableVertexAttribArray(a_Color);
 
